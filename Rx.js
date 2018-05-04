ca7980c4b2dadca0583a650b1a98cfce17c36bc8
zero1five <2642585002@qq.com>
2018-05-04 21:56:40 +0800

    new ES6 function

--- Rx.js
+++ Rx.js
@@ -10,3 +10,28 @@ const forEachObject = (obj, fn) => {
     }
 }
 
+/**
+ * @param {*} value - 返回的函数的参数
+ */
+const tap = (value) => 
+    (fn) => {
+        typeof(fn) === 'function' && fn(value)
+    }
+
+/**
+ * @param {*} fn - 只执行一次的函数
+ */
+const once = (fn) => {
+    let done = false;
+    return function () {
+        return done ? void 0 : ((done = true), fn.apply(this, arguments))
+    }
+}
+
+/**
+ * @param {*} fn - 多次递归的函数, 缓存每次计算的结果
+ */
+const memoized = (fn) => {
+    const lookupTable = {};
+    return (arg) => lookupTable[arg] || (lookupTable[arg] = fn(arg));
+}
