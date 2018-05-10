1184ebe6c4aea1f9ef12363e871bb0b086ff1ebe
zero1five <2642585002@qq.com>
2018-05-10 23:33:15 +0800

    :review:

--- test.js
+++ test.js
@@ -14,3 +14,21 @@ send();
 send(); 
 
 /* 函数节流 */
+
+let arr = [2,1,423432,4,4342342,1231,3232,123123,1231]
+
+function insertion(arr) {
+    for( let i = 1; i < arr.length; i++ ) {
+        key = arr[i]
+        j = i - 1
+        
+        while (j > -1 && arr[j] > key) {
+            arr[j + 1] = arr[j] // a[i] = arr[j]
+            j--
+            arr[j + 1] = key
+        }
+    }
+    return arr
+}
+
+console.log(insertion(arr))
