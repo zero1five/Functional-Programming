854d25c42b547879087b8ff7a4417fbd9504e7e6
zero1five <2642585002@qq.com>
2018-05-14 20:33:09 +0800

    :moon: curry-study

--- test.js
+++ test.js
@@ -98,3 +98,25 @@ var data = [
 data.forEach(v=>v.imgs=v.img.reduce((a,b)=>a.num+b.num))
 
 console.log(data)
+
+var cost = (function() {
+    var args = [];
+
+    return function() {
+        if(arguments.length === 0) {
+            var money = 0;
+            for (let i = 0; l = args.length, i < l; i++) {
+                money += args[i]
+            }
+            return money;
+        } else {
+            [].push.apply(args, arguments)
+        }
+    }
+})()
+
+cost(100);
+cost(200);
+cost(300);
+
+console.log(cost());
