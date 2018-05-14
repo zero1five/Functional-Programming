cae8b5eac2ac5b35e1bbf9e0cd2ce62d139fb398
zero1five <2642585002@qq.com>
2018-05-14 20:39:03 +0800

    :moon: curry + (fn)

--- test.js
+++ test.js
@@ -105,7 +105,7 @@ var cost = (function() {
     return function() {
         if(arguments.length === 0) {
             var money = 0;
-            for (let i = 0; l = args.length, i < l; i++) {
+            for (let i = 0, l = args.length; i < l; i++) {
                 money += args[i]
             }
             return money;
@@ -120,3 +120,34 @@ cost(200);
 cost(300);
 
 console.log(cost());
+
+var currying = function(fn) {
+    var args = [];
+    
+    return function() {
+        if (arguments.length === 0) {
+           return fn.apply(this, args) 
+        } else {
+            [].push.apply(args, arguments);
+            return arguments.callee
+        }
+    }
+}
+
+var cost = (function() {
+   var money = 0;
+    return function() {
+        for ( let i = 0, l = arguments.length; i < l; i++ ) {
+            money += arguments[i]
+        }
+        return money
+    } 
+})();
+
+var cost = currying(cost);
+
+cost(100);
+cost(200);
+cost(300);
+
+console.log(cost())
