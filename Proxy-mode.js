04bb497c6dc17dfa9317668b7c198c2745942c1f
zero1five <2642585002@qq.com>
2018-05-18 21:59:10 +0800

    :moon: 代理模式学习

--- Proxy-mode.js
+++ Proxy-mode.js
@@ -1,11 +1,10 @@
 var myImage = (function() {
-    var imgNodes = document.querySelectorAll('img');
-    
+    var imgNode = document.createElement('img');
+    document.body.appendChild(imgNode);
+
     return {
         setSrc: function( src ) {
-            for ( let i = 0, l = imgNodes.length; i < l; i++ ) {
-                imgNodes[i].src = src
-            }
+            imgNode.src = src;
         }
     }
 })();
@@ -17,10 +16,46 @@ var proxyImage = (function(){
     }
     return {
         setSrc: function( src ) {
-            myImage.setSrc('./loading.gif');
-            setTimeout(() =>  img.src = src, 1000)
+            myImage.setSrc('./loading.gif')
+            setTimeout(() => {
+                img.src = src
+            }, 1000);
         }
     }
 })();
 
 proxyImage.setSrc('http://zero1five.gitee.io/pic/M/004.png')
+
+
+/**
+ ***************** 虚拟代理 - 惰性加载 **********************
+ */
+
+var miniConsole = (function() {
+    var cache = [];
+    var handler = function( ev ) {
+        if ( ev.keyCode === 113 ) {
+            var script = document.createElement( 'script' );
+            script.onload = function(){
+                for ( let i = 0, fn; fn = cache[ i++ ]; ) {
+                    fn();
+                }
+            };
+            script.scr = 'Ps-mode.js';
+            document.getElementsByName( 'head' )[0].appendChild( script );
+            document.body.removeEventListener( 'keydown', handler );
+        }
+    };
+    
+    document.body.addEventListener( 'keydown', handler, false );
+
+    return {
+        log: function() {
+            var args = arguments;
+                cache.push( function() {
+                    
+                })
+        }
+    }
+})();
+
