765d23c731ddad981ce065ee0ee6bb6664cd4415
zero1five <2642585002@qq.com>
2018-05-20 21:16:34 +0800

    new QuickSort

--- QuickSort.js
+++ QuickSort.js
@@ -3,16 +3,35 @@ let arr = [1,23,19,5,56,23,123.5,-10,98,56,10,10,0,-20,123,201,123];
  * 
  */
 var quickSort = function(arr) {
-    if ( arr.length <= 1) return arr
-    let pivotIndex = Math.floor(arr.length / 2),
-        pivot = arr.splice(pivotIndex, 1)[0],
-        left = [],
-        right = [];
-
-    for ( let i = 0, l = arr.length; i < l; i++ ) {
-        arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i])
+    function swap( arr, i, k ) {
+        var temp = arr[i];
+        arr[i] = arr[k];
+        arr[k] = temp;
     }
-    return quickSort(left).concat([pivot], quickSort(right))
+
+    function partion( arr, left, right ) {
+        var storeIndex = left;
+        var pivot = arr[right];
+        for ( let i = left; i < right; i++ ) {
+            if ( arr[i] < pivot ) {
+                swap(arr, storeIndex, i);
+                storeIndex++;
+            }
+        }
+        swap(arr, right, storeIndex);
+        return storeIndex
+    }
+
+    function sort(arr, left, right) {
+        if ( left > right ) return
+        var storeIndex = partion(arr, left, right);
+        sort(arr, left, storeIndex - 1);
+        sort(arr, storeIndex + 1, right)
+    }
+    
+    sort(arr, 0, arr.length - 1);
+    return arr
 }
 
 console.log(quickSort(arr))
+console.log(arr)
